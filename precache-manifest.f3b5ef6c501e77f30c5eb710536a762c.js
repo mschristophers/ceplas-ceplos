336a4bf19a3d1b8ff70ae0c3ec4c09d1ae63f4c9
Manuel Stefan Christopher <manuelstefan150@gmail.com>
2020-08-22 16:59:06 +0700

    Updates

---
+++ precache-manifest.f3b5ef6c501e77f30c5eb710536a762c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7466c91a8f062b6894778d9aa55918eb",
+    "url": "/ceplas-ceplos/index.html"
+  },
+  {
+    "revision": "87bd0dc2d56ada84c7b1",
+    "url": "/ceplas-ceplos/static/css/main.c4cacc22.chunk.css"
+  },
+  {
+    "revision": "0f385bedadc260a0d242",
+    "url": "/ceplas-ceplos/static/js/2.66e63762.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/ceplas-ceplos/static/js/2.66e63762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bd0dc2d56ada84c7b1",
+    "url": "/ceplas-ceplos/static/js/main.3c55ea03.chunk.js"
+  },
+  {
+    "revision": "8f1c562650eefc71bcfb",
+    "url": "/ceplas-ceplos/static/js/runtime-main.91620c95.js"
+  }
+]);
